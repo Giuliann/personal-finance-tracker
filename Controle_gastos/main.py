766fc1f69531f3fbe5d42d42b1d9b70d155ce6bd
Giuliann de Sousa Duarte <sgiuliann@gmail.com>
2025-12-26 13:48:49 -0300

    Update menu

--- Controle_gastos/main.py
+++ Controle_gastos/main.py
@@ -1,7 +1,6 @@
 ### Inicio do Programa ###
 from storage import add_user, remove_user, add_categoria, remove_categoria, add_registro, remove_registro, lista_gastos
-from relatorio import relatorio_pessoa
-
+from relatorio import relatorio_pessoa, relatorio_total
 def menu():
     print('--' * 25)
     print('Bem vindo ao menu do Sistema de Controle de Gastos')
@@ -11,73 +10,87 @@ def menu():
     ### Menu de Interação ###
     while True:
         print('Escolha uma opção:')
-        print('(0) Inserir Novos Usuarios e novas Categorias ')
-        print('(1) Inserir Novo Gasto')
-        print('(2) Listar Gastos')
-        print('(3) Relatorio Geral')
-        print('(4) Remover Gasto, Usuarios e Categorias')
-        print('(5) Fechar Programa...')
+        print('[1] Adicionar ')
+        print('[2] Remover ')
+        print('[3] Novo Gasto ')
+        print('[4] Relatorio ')
+        print('[5] Listar todos os gastos')
+        print('[6] Fechar Programa')
 
         select = int(input('> '))
         match select:
-            case 0:
-                print('Você Gostaria de Adicionar um novo Usuario ou uma nova Categoria ?')
-                print('[1] Novo Usuario')
-                print('[2] Nova Categoria')
-                print('[3] Voltar Para o Menu')
-
-                select = int(input('> '))
-                match select:
-                    case 1:
-                        add_user()
-                        input('\nPresione ENTER para continuar...\n\n')
-
-                    case 2:
-                        add_categoria()
-                        input('\nPresione ENTER para continuar...\n\n')
-
-                    case 3:
-                        menu()
-
             case 1:
-                print('Você Gostaria de Adicionar um novo Gasto ?')
-                print('[1] Adicionar novo Gasto')
-                print('[2] Voltar para o Menu')
+                print('O que gostaria de adicionar?')
+                print('[1] Pessoa ')
+                print('[2] Categoria ')
                 select = int(input('> '))
-
-                match select:
-
-                    case 1:
-                        add_registro()
-                        input('\nPresione ENTER para continuar...\n\n')
-
-                    case 2: 
-                        menu()
-
+                if select == 1:
+                    add_user()
+                elif select == 2:
+                    add_categoria()
+                else:
+                    print('Não existe essa opção')
+                    print('Voltando para menu...')
+                    menu()
             case 2:
-                lista_gastos()
-                input('\nPresione ENTER para continuar...\n\n')
-                
-            case 3:
-                relatorio_pessoa()
-            case 4:
-                print('O que você deseja remover:')
-                print('[1] Remover Usuario')
-                print('[2] Remover Categoria')
-                print('[3] Remover Registro')
+                print('O que gostaria de remover?')
+                print('[1] Pessoa ')
+                print('[2] Categoria ')
+                print('[3] Registro ')
                 select = int(input('> '))
-                match select:
-                    case 1:
-                        remove_user()
-                    case 2:
-                        remove_categoria()
-                    case 3:
-                        remove_registro()
+                if select == 1:
+                    remove_user()
+                elif select == 2:
+                    remove_categoria()
+                elif select == 3:
+                    remove_registro()
+                else:
+                    print('Não existe essa opção')
+                    print('Voltando para menu...')
+                    menu()
+            case 3:
+                print('Deseja adicionar um novo gasto?')
+                print('[1] Sim')
+                print('[2] Não')
+                select = int(input('> '))
+
+                if select == 1:
+                    add_registro()
+                elif select == 2:
+                    menu()
+                else:
+                    print('Não existe essa opção')
+                    print('Voltando para menu...')
+                    menu()
+            case 4:
+                print('Você gostaria de vizualizar o relatorio: ')
+                print('[1] Pessoal ')
+                print('[2] Total ')
+                select = int(input('> '))
+                if select == 1:
+                    relatorio_pessoa()
+                elif select == 2:
+                    relatorio_total()
+                else:
+                    print('Não existe essa opção')
+                    print('Voltando para menu...')
+                    menu()
             case 5:
-                exit('Fim do Progama')
-
+                print('Gostaria de listar todos os gastos?')
+                print('[1] Sim ')
+                print('[2] Não ')
+                select = int(input('> '))
+                if select == 1:
+                    lista_gastos()
+                elif select == 2:
+                    menu()
+                else:
+                    print('Não existe essa opção')
+                    print('Voltando para menu...')
+                    menu()
+            case 6:
+                exit('Fim do Programa...')
             case _:
-                print('O número que você digitou não representa nenhuma das opções do menu')
-
-        
+                print('Função não existe')
+                print('Tente novamente...')
 menu()
