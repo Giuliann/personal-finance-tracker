a0af6f0d6ee4628ad545e67dd30e842d18d344ca
Giuliann de Sousa Duarte <sgiuliann@gmail.com>
2026-01-01 19:43:54 -0300

    Implementação de utils constantes
    um arquivo com variáveis constantes para ajudar durante o desenvolvimento.

---
+++ Controle_gastos/utils.py
@@ -0,0 +1,28 @@
+from datetime import date
+
+# Tempo de pesquisa maximo: 
+def futuro():
+    ano_futuro = date.today().year + 2
+    return ano_futuro
+
+# Tempo de pesquisa minima:
+def passado():
+    ano_passado = date.today().year - 20
+    return ano_passado
+
+
+# Consulta de meses: 
+def meses():
+    meses = { 1: 'Janeiro', 
+          2: 'Fevereiro',
+          3: 'Março',
+          4: 'Abril',
+          5: 'Maio',
+          6: 'Junho',
+          7: 'Julho',
+          8: 'Agosto',
+          9: 'Setembro',
+         10: 'Outubro',
+         11: 'Novembro',
+         12: 'Dezembro'}
+    return meses
